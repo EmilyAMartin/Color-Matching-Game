427652260528f61645f10b00ec94f60e823426e8
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-07-04 22:15:01 -0230

    Working on card flip function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Color-Matching-Game
+## Color-Matching-Game Demo
+Check out the game here https://emilyamartin.github.io/Color-Matching-Game/
+
+## Motivation
+I made this little color matching game to practice using grids and arrays. 
+
+## Credits
--- main.js
+++ main.js
@@ -1,66 +1,71 @@
 const tilesContainer = document.querySelector(".tiles");
-const colors = ["#001219", "#0a9396", "#94D2BD", "#EE9B00", "#CA6702", "#BB3E03", "#AE2012", "#9B2226"];
-const colorsPicklist = [...colors, ...colors];
-const tileCount = colorsPicklist.length;
+const colors = [
+  "#001219",
+  "#0a9396",
+  "#94D2BD",
+  "#EE9B00",
+  "#CA6702",
+  "#BB3E03",
+  "#AE2012",
+  "#9B2226",
+];
+const colorsList = [...colors, ...colors];
+const tileCount = colorsList.length;
 
-let revealedCount = 0
+let revealedCount = 0;
 let activeTile = null;
 let awaitingEndOfMove = false;
 
-
 function buildTile(color) {
-    const element = document.createElement("div");
-    element.classList.add("tile");
-    element.setAttribute("data-color", color);
-    element.setAttribute("data-revealed", "false");
-    element.addEventListener ("click", () =>{
-    const revealed = element.getAttribute("data-revealed")
-        if (awaitingEndOfMove
-            || revealed === "true"
-            || element === activeTile
-        ){
-            return;
-        }
+  const element = document.createElement("div");
+  element.classList.add("tile");
+  element.setAttribute("data-color", color);
+  element.setAttribute("data-revealed", "false");
+  element.addEventListener("click", () => {
+    const revealed = element.getAttribute("data-revealed");
+    if (awaitingEndOfMove || revealed === "true" || element === activeTile) {
+      return;
+    }
     element.style.backgroundColor = color;
-        if (!activeTile){
-            activeTile = element;
-            return;
-        }
+    if (!activeTile) {
+      activeTile = element;
+      return;
+    }
     const colorToMatch = activeTile.getAttribute("data-color");
-        if (colorToMatch === color) {
-            activeTile.setAttribute("data-revealed", "true")
-            element.setAttribute("data-revealed", "true")
-            awaitingEndOfMove = false;
-            activeTile= null;
-            revealedCount += 2;
+    if (colorToMatch === color) {
+      activeTile.setAttribute("data-revealed", "true");
+      element.setAttribute("data-revealed", "true");
+      awaitingEndOfMove = false;
+      activeTile = null;
+      revealedCount += 2;
 
-        if (revealedCount === tileCount) {
-            document.getElementById("win-msg").style.display = "block";
-            }
-            return;
-        }
+      if (revealedCount === tileCount) {
+        document.getElementById("win-msg").style.display = "block";
+      }
+      return;
+    }
 
-        awaitingEndOfMove = true;
-        setTimeout(() => {
-            element.style.backgroundColor = null;
-            activeTile.style.backgroundColor = null;
-            awaitingEndOfMove = false;
-            activeTile = null;
-        }, 1000);
-    });
+    awaitingEndOfMove = true;
+    setTimeout(() => {
+      element.style.backgroundColor = null;
+      activeTile.style.backgroundColor = null;
+      awaitingEndOfMove = false;
+      activeTile = null;
+    }, 1000);
+  });
 
-    return element;
+  return element;
 }
 
 //Tiles//
 for (let i = 0; i < tileCount; i++) {
-    const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
-    const color = colorsPicklist[randomIndex];
-    const tile = buildTile(color)
+  const randomIndex = Math.floor(Math.random() * colorsList.length);
+  const color = colorsList[randomIndex];
+  const tile = buildTile(color);
 
-    colorsPicklist.splice(randomIndex, 1); //Max two colors//
-    tilesContainer.appendChild(tile);
+  colorsList.splice(randomIndex, 1); //Max two colors//
+  tilesContainer.appendChild(tile);
 }
-function restart(){
-    window.location.reload()
+function restart() {
+  window.location.reload();
 }
